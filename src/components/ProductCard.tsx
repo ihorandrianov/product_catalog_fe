ba5884b142ceb8444cd617b301c363127ffaf1f0
Oleksandr Korobko <oleksandr.korobko@gmail.com>
2022-12-01 15:52:50 +0200

    implemented layout and start working with styles

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,6 +2,9 @@ import { Phones } from '@prisma/client';
 import Image from 'next/image';
 import { FC } from 'react';
 import productStyles from '../styles/ProductCard.module.css';
+import typographyMobile from '../styles/TypographyMobile.module.css';
+import typographyDesktop from '../styles/TypographyDesktop.module.css';
+import colors from '../styles/Colors.module.css';
 
 type Props = {
   product: Phones;
@@ -10,13 +13,67 @@ type Props = {
 export const ProductCard: FC<Props> = ({ product }) => {
   return (
     <div className={productStyles.card}>
-      <Image
+      <Image 
+        className={productStyles.card__img}
         src={`/${product.image}`}
         alt={product.name}
         width={208}
         height={196}
       />
-      <p>Hello</p>
+
+      <h2 className={`${productStyles.card__title} ${typographyDesktop.bodyText}`}>
+        {product.name}
+      </h2>
+
+      <p className={productStyles.card__price}>
+        <span className={productStyles.card__priceСurrent}
+          >
+          {`$${product.price}`}
+        </span>
+
+        <span className={productStyles.card__priceFull}>
+          {`$${product.fullPrice}`}
+        </span>
+      </p>
+
+      <div className={productStyles.card__features}>
+        <p className={productStyles.card__feature}>
+          <span className={productStyles.card__featureName}>
+            Screen
+          </span>
+          
+          <span className={productStyles.card__featureValue}>
+            {product.screen}
+          </span>
+        </p>
+
+        <p className={productStyles.card__feature}>
+          <span className={productStyles.card__featureName}>
+            Capacity
+          </span>
+          
+          <span className={productStyles.card__featureValue}>
+            {product.capacity}
+          </span>
+        </p>
+
+        <p className={productStyles.card__feature}>
+          <span className={productStyles.card__featureName}>
+            RAM
+          </span>
+          
+          <span className={productStyles.card__featureValue}>
+            {product.ram}
+          </span>
+        </p>
+      </div>
+
+      <a 
+        className={productStyles.card__price}
+        href="#"
+      >
+        Add to cart
+      </a>
     </div>
   );
 };

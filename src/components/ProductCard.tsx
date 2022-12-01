6fa416ff65cf0d3bef4e7a558f413b0e18df0219
Oleksandr Korobko <oleksandr.korobko@gmail.com>
2022-12-02 00:41:20 +0200

    implemented styles

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,8 +1,8 @@
 import { Phones } from '@prisma/client';
 import Image from 'next/image';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import classNames from 'classnames';
 import productStyles from '../styles/ProductCard.module.css';
-import typographyMobile from '../styles/TypographyMobile.module.css';
 import typographyDesktop from '../styles/TypographyDesktop.module.css';
 import colors from '../styles/Colors.module.css';
 
@@ -11,6 +11,9 @@ type Props = {
 };
 
 export const ProductCard: FC<Props> = ({ product }) => {
+  const [added, setAdded] = useState(false);
+  const [favorite, setFavorite] = useState(false);
+  
   return (
     <div className={productStyles.card}>
       <Image 
@@ -21,28 +24,27 @@ export const ProductCard: FC<Props> = ({ product }) => {
         height={196}
       />
 
-      <h2 className={`${productStyles.card__title} ${typographyDesktop.bodyText}`}>
+      <h2 className={`${productStyles.card__title } ${typographyDesktop.bodyText}`}>
         {product.name}
       </h2>
 
       <p className={productStyles.card__price}>
-        <span className={productStyles.card__priceСurrent}
-          >
+        <span className={`${productStyles.card__priceСurrent} ${typographyDesktop.h3Desktop}`}>
           {`$${product.price}`}
         </span>
 
-        <span className={productStyles.card__priceFull}>
+        <span className={`${productStyles.card__priceFull} ${typographyDesktop.h3Desktop}`}>
           {`$${product.fullPrice}`}
         </span>
       </p>
 
       <div className={productStyles.card__features}>
         <p className={productStyles.card__feature}>
-          <span className={productStyles.card__featureName}>
+          <span className={`${productStyles.card__featureName} ${typographyDesktop.smallText}`}>
             Screen
           </span>
           
-          <span className={productStyles.card__featureValue}>
+          <span className={`${productStyles.card__featureValue} ${typographyDesktop.smallText}`}>
             {product.screen}
           </span>
         </p>
@@ -68,12 +70,28 @@ export const ProductCard: FC<Props> = ({ product }) => {
         </p>
       </div>
 
-      <a 
-        className={productStyles.card__price}
-        href="#"
-      >
-        Add to cart
-      </a>
+      <div className={productStyles.card__buy}>
+        <a
+          onClick={() => {
+            setAdded(!added);
+          }}
+          className={classNames(`${productStyles.card__addToCart}`, 'buttons', {
+            'card__addToCartActive': added,
+          })}
+        >
+          {added ? 'Added' : 'Add to cart'}
+        </a>
+
+        <a
+          onClick={() => {
+            setFavorite(!favorite);
+          }}
+          className={classNames(`${productStyles.card__favoritesIcon}`, {
+            'card__favoritesIconActive': favorite,
+          })}
+        >
+        </a>
+      </div>
     </div>
   );
 };

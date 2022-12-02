cf8374c5319a35d1ddb7f3ce95b7de3bf21f8e27
Ihor Andrianov <andrianoia@gmail.com>
2022-12-02 23:26:58 +0200

    fix: fixed links in header | added header to pages

--- src/pages/phones/[id].tsx
+++ src/pages/phones/[id].tsx
@@ -14,6 +14,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import PhoneInfo from '../../components/PhoneInfo';
+import Header from '../../components/Header';
 
 const isSSR = () => typeof window === 'undefined';
 
@@ -69,6 +70,7 @@ function PhonePage(props: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
     <main>
+      <Header />
       {data && <PhoneInfo phones={data} />}
       <Footer />
     </main>

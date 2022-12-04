b9d0012e2ee9b76e5cbc05535b2ae2caf6a76836
Vladimir Dyblia <vladimirdublya@gmail.com>
2022-12-04 11:17:07 +0200

    added nextjs navigation

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import Link from 'next/link';
-import styles from '../styles/TypographyDesktop.module.css'; 
 import position from '../styles/404.module.css';
 import Image from 'next/image';
 import logo from '../../public/icons/Logo.svg';
-import { useNavigate } from 'react-router-dom';
+import { useRouter } from 'next/router'
 
 export default function Custom404() {
-    const navigate = useNavigate();
-
-    const goBack = () => navigate(-1);
+    const router = useRouter()
 
     return (
         <>
@@ -19,8 +15,8 @@ export default function Custom404() {
                 className={position.logo}
             />
             <div className={position.position}>
-            <h1 className={styles.h1Desktop}>Sorry... This page does not found (404)</h1>
-            <button type="button" onClick={goBack}> 
+            <h1 className={position.h1}>Sorry... This page does not found (404)</h1>
+            <button onClick={() => router.back()} className={`${position.primaryButton} `}>
                 Go previous page 
             </button>
             </div>

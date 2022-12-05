78e38acbcd2372653e49c7ab2b64ed32f86a67dc
Ihor Andrianov <andrianoia@gmail.com>
2022-12-05 11:44:47 +0200

    feat: add auth

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,10 +1,12 @@
 import { router } from '../trpc';
+import { cartRouter } from './cart';
 import { detailsRouter } from './details';
 import { productsRouter } from './products';
 
 export const appRouter = router({
   products: productsRouter,
   details: detailsRouter,
+  cart: cartRouter,
 });
 
 export type AppRouter = typeof appRouter;

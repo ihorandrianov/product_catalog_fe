0ab65bb0372e9280ea7331fa7a29ebb27841547f
Oleksandr Korobko <oleksandr.korobko@gmail.com>
2022-12-08 23:14:34 +0200

    almost done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header';
 import classNames from 'classnames';
 import typography from '../styles/Typography.module.css';
+import homeStyles from '../styles/Home.module.css';
 import { Footer } from '../components/Footer';
 import Head from 'next/head';
 import { Recomended } from '../components/Recomended';
@@ -15,12 +16,15 @@ export default function Home() {
         <meta name="description" content="Main page of NiceGadgets" />
       </Head>
       <Header />
-      <h1 className={classNames('mainTitle', typography.h1)}>
-        Welcome to Nice Gadgets store!
-      </h1>
-      <HomePageTopSlider />
-      <Recomended title="Brand new models" order="new" />
-      <Recomended title="Hot prices" order="hot" />
+      
+      <div className={homeStyles.mainWrapper}>
+        <h1 className={classNames(homeStyles.mainTitle, typography.h1)}>
+          Welcome to Nice Gadgets store!
+        </h1>
+        <HomePageTopSlider />
+        <Recomended title="Brand new models" order="new" />
+        <Recomended title="Hot prices" order="hot" />
+      </div>
       <Footer />
     </>
   );

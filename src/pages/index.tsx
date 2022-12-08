ea093fb311ab4e3e86e9c1f39e4890f170ca6a91
Oleksandr Korobko <oleksandr.korobko@gmail.com>
2022-12-08 18:43:43 +0200

    implemented HomePageTopSlider but need to finish styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import Header from '../components/Header';
+import classNames from 'classnames';
+import typography from '../styles/Typography.module.css';
 import { Footer } from '../components/Footer';
 import Head from 'next/head';
 import { Recomended } from '../components/Recomended';
+import { HomePageTopSlider } from '../components/HomePageTopSlider';
 
 export default function Home() {
   return (
@@ -12,6 +15,10 @@ export default function Home() {
         <meta name="description" content="Main page of NiceGadgets" />
       </Head>
       <Header />
+      <h1 className={classNames('mainTitle', typography.h1)}>
+        Welcome to Nice Gadgets store!
+      </h1>
+      <HomePageTopSlider />
       <Recomended title="Brand new models" order="new" />
       <Recomended title="Hot prices" order="hot" />
       <Footer />
